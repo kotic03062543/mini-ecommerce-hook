1a59a2d2bc135db8bda04a133baec55bfedaf95d
Wanchaloem <wanchaloem.phr@axonstech.com>
2025-06-02 15:57:39 +0700

    feat(about): enhance About page layout and content with images and statistics

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,9 +1,75 @@
+import MeAiImage from "../assets/MeAi.jpeg";
+import MeCutImage from "../assets/MeCut.png";
+
 function About() {
   return (
-    <div className="flex flex-col w-full h-screen justify-center items-center">
-      <h1 className="text-2xl font-bold">About</h1>
-      <p>เว็บตัวอย่าง e-commerce เรียน React</p>
-    </div>
+    <section className="py-24 relative">
+      <div className="w-full max-w-7xl px-4 md:px-5 lg:px-5 mx-auto">
+        <div className="w-full justify-start items-center gap-12 grid lg:grid-cols-2 grid-cols-1">
+          <div className="w-full justify-center items-start gap-6 grid sm:grid-cols-2 grid-cols-1 lg:order-first order-last">
+            <div className="pt-24 lg:justify-center sm:justify-end justify-start items-start gap-2.5 flex">
+              <img
+                className=" rounded-xl object-cover"
+                src={MeAiImage}
+                alt="about Us image"
+              />
+            </div>
+            <img
+              className="sm:ml-0 ml-auto rounded-xl object-cover"
+              src={MeCutImage}
+              alt="about Us image"
+            />
+          </div>
+          <div className="w-full flex-col justify-center lg:items-start items-center gap-10 inline-flex">
+            <div className="w-full flex-col justify-center items-start gap-8 flex">
+              <div className="w-full flex-col justify-start lg:items-start items-center gap-3 flex">
+                <h2 className="text-gray-900 text-4xl font-bold font-manrope leading-normal lg:text-start text-center">
+                  Website for lerning React and Tailwind CSS
+                </h2>
+                <p className="text-gray-500 text-base font-normal leading-relaxed lg:text-start text-center">
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Maxime fugit eius a, dignissimos praesentium architecto
+                  tempore eum cupiditate porro molestiae necessitatibus nisi,
+                  exercitationem illo iure unde error? Qui, inventore
+                  exercitationem.
+                </p>
+              </div>
+              <div className="w-full lg:justify-start justify-center items-center sm:gap-10 gap-5 inline-flex">
+                <div className="flex-col justify-start items-start inline-flex">
+                  <h3 className="text-gray-900 text-4xl font-bold font-manrope leading-normal">
+                    3+
+                  </h3>
+                  <h6 className="text-gray-500 text-base font-normal leading-relaxed">
+                    Years of Experience
+                  </h6>
+                </div>
+                <div className="flex-col justify-start items-start inline-flex">
+                  <h4 className="text-gray-900 text-4xl font-bold font-manrope leading-normal">
+                    20+
+                  </h4>
+                  <h6 className="text-gray-500 text-base font-normal leading-relaxed">
+                    Successful Projects
+                  </h6>
+                </div>
+                <div className="flex-col justify-start items-start inline-flex">
+                  <h4 className="text-gray-900 text-4xl font-bold font-manrope leading-normal">
+                    15+
+                  </h4>
+                  <h6 className="text-gray-500 text-base font-normal leading-relaxed">
+                    Happy Clients
+                  </h6>
+                </div>
+              </div>
+            </div>
+            <button className="sm:w-fit w-full px-3.5 py-2 bg-indigo-600 hover:bg-indigo-800 transition-all duration-700 ease-in-out rounded-lg shadow-[0px_1px_2px_0px_rgba(16,_24,_40,_0.05)] justify-center items-center flex">
+              <span className="px-1.5 text-white text-sm font-medium leading-6">
+                Read More
+              </span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
