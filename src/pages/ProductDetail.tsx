642706142149cb4194d310e7dbf8fef441239f32
Wanchaloem <wanchaloem.phr@axonstech.com>
2025-06-04 16:08:43 +0700

    feat(breadcrumb): implement Breadcrumb component and integrate into Home and ProductDetail pages

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -2,8 +2,10 @@ import { useParams } from "react-router-dom";
 import { serviceGetProductDetails } from "../services/ProductDetails/index";
 import { useQuery } from "@tanstack/react-query";
 import useProductsViewModel from "./Products/ProductsViewModel";
+import Breadcrumb from "../components/Breadcrumb";
 
 function ProductDetail() {
+  // console.log("ProductDetail");
   const viewModel = useProductsViewModel();
 
   if (!viewModel) {
@@ -20,6 +22,12 @@ function ProductDetail() {
     enabled: !!productId,
   });
 
+  const breadcrumbItems = [
+    { label: "Home", link: "/" },
+    { label: "ProductDetail", link: `/ProductDetail/${productId}` },
+    { label: product?.title, link: "#" },
+  ];
+
   if (isLoading) {
     return (
       <div className="p-5 md:p-20 grid md:flex gap-6 animate-pulse">
@@ -34,29 +42,35 @@ function ProductDetail() {
     );
   }
   return (
-    <div className="p-5 md:p-20 grid md:flex gap-6">
-      <img
-        src={product.image}
-        alt={product.title}
-        className="w-full px-5 md:w-1/2 espect-[4/3] object-cover rounded-lg mb-5 md:mb-0 md:aspect-[4/3] md:h-full md:max-w-[400px] mx-auto"
-      />
-      <div className="w-full flex flex-col gap-3">
-        <p className="text-2xl font-bold">{product.title}</p>
-        <p className="text-2xl font-semibold">
-          <span className="text-red-500">{product.price}</span> $
-        </p>
-        <div className="mt-6">
-          <h3 className="text-sm font-medium text-gray-900">Description</h3>
-          <p className="mt-2 text-sm text-gray-700">{product.description}</p>
-        </div>
-        <button
-          onClick={() => updateCartWithProduct(product)}
-          className="mt-5 w-full bg-purple-600 text-white py-3 rounded hover:bg-purple-700"
-        >
-          Add to cart
-        </button>
+    <>
+      <div className="flex justify-center items-center mt-5">
+        <Breadcrumb items={breadcrumbItems} />
       </div>
-    </div>
+
+      <div className="p-5 md:p-20 grid md:flex gap-6">
+        <img
+          src={product.image}
+          alt={product.title}
+          className="w-full px-5 md:w-1/2 espect-[4/3] object-cover rounded-lg mb-5 md:mb-0 md:aspect-[4/3] md:h-full md:max-w-[400px] mx-auto"
+        />
+        <div className="w-full flex flex-col gap-3">
+          <p className="text-2xl font-bold">{product.title}</p>
+          <p className="text-2xl font-semibold">
+            <span className="text-red-500">{product.price}</span> $
+          </p>
+          <div className="mt-6">
+            <h3 className="text-sm font-medium text-gray-900">Description</h3>
+            <p className="mt-2 text-sm text-gray-700">{product.description}</p>
+          </div>
+          <button
+            onClick={() => updateCartWithProduct(product)}
+            className="mt-5 w-full bg-purple-600 text-white py-3 rounded hover:bg-purple-700"
+          >
+            Add to cart
+          </button>
+        </div>
+      </div>
+    </>
   );
 }
 
